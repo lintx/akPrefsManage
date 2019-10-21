f3406d00a402e72e19159743522352e8e34334d4
lintx <lintx@lintx.org>
2019-10-21 10:35:53 +0800

    修复部分系统因为chmod命令不识别o+rw模式而导致无法使用的问题

--- app/src/main/java/org/lintx/akprefsmanage/utils/FilePermission.kt
+++ app/src/main/java/org/lintx/akprefsmanage/utils/FilePermission.kt
@@ -4,22 +4,22 @@ import java.io.DataOutputStream
 
 class FilePermission(private val file:String) {
     fun setRead():Boolean{
-        return changeFilePermission("+r")
+        return changeFilePermission("664")
     }
 
     fun setReadWrite():Boolean{
-        return changeFilePermission("+rw")
+        return changeFilePermission("666")
     }
 
     fun cancel():Boolean{
-        return changeFilePermission("-rw")
+        return changeFilePermission("660")
     }
 
     private fun changeFilePermission(permission:String):Boolean{
         var process: Process? = null
         var os: DataOutputStream? = null
         try {
-            val cmd = "chmod o$permission $file"
+            val cmd = "chmod $permission $file"
             process = Runtime.getRuntime().exec("su") //切换到root帐号
             os = DataOutputStream(process!!.outputStream)
             os.writeBytes(cmd + "\n")
